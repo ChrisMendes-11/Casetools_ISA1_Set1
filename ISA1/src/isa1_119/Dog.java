2e6b95bb9968e9cbf42bdf13ec34bf8a3960cbad
exam119 <rbca22119.rcca@gmail.com>
2024-02-07 10:32:17 +0530

    initial commit

---
+++ ISA1/src/isa1_119/Dog.java
@@ -0,0 +1,16 @@
+package isa1_119;
+
+public class Dog {
+	private String name;
+	private String breed;
+	public Dog(String name,String breed)
+	{
+		this.name = name;
+		this.breed = breed;
+	}
+	public String getBreed()
+	{
+		return breed;
+	}
+
+}
